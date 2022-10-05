a3aa28d4613d18cb95340ed9e0d0f964e8a24474
joshdoyle94 <joshdoyle94@gmail.com>
2022-10-05 00:22:59 -0400

    initial changes to movieControllers and server cleanup

--- server.js
+++ server.js
@@ -8,25 +8,6 @@ const mongoose = require('mongoose') // import mongoose
 const path = require('path') // import path module
 const Movie = require('./models/movie')
 
-/////////////////////////////////////////////
-// Database Connection
-/////////////////////////////////////////////
-// Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-
-// Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG)
-
-// Events for when connection opens/disconnects/errors
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error))
-
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
